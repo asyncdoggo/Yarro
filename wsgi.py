9f3adfae3e84edf1761deca0c55ac135efc9a228
asyncdoggo <ayushdeshpande81@gmail.com>
2022-11-07 15:19:29 +0530

    Test

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+# wsgi.py
+# Set the path
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from app import app
+
+if __name__ == "__main__":
+    app.run()

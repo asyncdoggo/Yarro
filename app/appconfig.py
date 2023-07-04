42dc73ea3de3cfe417eb4751b95668ff4ba19228
asyncdoggo <ayushdeshpande81@gmail.com>
2023-07-04 18:34:00 +0530

    Add support for sqlite engine

--- app/appconfig.py
+++ app/appconfig.py
@@ -1,13 +1,21 @@
 from dotenv import dotenv_values
-
+import os
 config = dotenv_values(".env")
 
-HOST=config["HOST"]
-USERNAME=config["USERNAME"]
-PASSWORD=config["PASSWORD"]
-DATABASE=config["DATABASE"]
+HOST = config["HOST"]
+USERNAME = config["USERNAME"]
+PASSWORD = config["PASSWORD"]
+DATABASE = config["DATABASE"]
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DEBUG = False
-SQLALCHEMY_DATABASE_URI = f'mysql://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE}?ssl=true'
+SQLALCHEMY_DATABASE_URI = ""
+
+if config["DB_ENGINE"] == "mysql":
+    SQLALCHEMY_DATABASE_URI = f'mysql://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE}?ssl=true'
+
+elif config["DB_ENGINE"] == "sqlite":
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.getcwd()}/data.db"
+
+
 SECRET_KEY = config["SECRET_KEY"]

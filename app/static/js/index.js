2f231de4aa3656749bb8699b0e89fdf2784aa2db
Ayush Deshpande <93706892+asyncdoggo@users.noreply.github.com>
2023-08-15 03:22:22 +0000

    Updates

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,5 +1,33 @@
 
 
+document.getElementById("login_form").addEventListener("submit", async function (e) {
+    document.getElementById("login_btn").disabled = true
+    e.preventDefault();
+    const form = new FormData(e.target);
+    const data = Object.fromEntries(form.entries());
+
+    const response = await fetch("/api/login", {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            "Authorization": "Basic " + btoa(data["uname"] + ":" + data["passwd"])
+        }
+    }).then((response) => response.json())
+
+    let status = response.status;
+    if (status == "success") {
+        localStorage.setItem("uname", response.uname);
+        localStorage.setItem("uid", response.uid);
+        window.location.reload()
+    }
+    else {
+        Snackbar.show({ pos: "bottom-center", text: response.status })
+    }
+    document.getElementById("login_btn").disabled = false
+
+})
+
 
 document.getElementById("passwd").addEventListener("focusin", (e) => {
     document.getElementById("pass").style.outlineWidth = "2px"
